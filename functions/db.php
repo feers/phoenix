03ff302306bd52576c6f077b5846fde0fd9507da
Viskander <valeeviskander@gmail.com>
2013-05-27 21:01:26 +0400

    Update db.php

--- functions/db.php
+++ functions/db.php
@@ -1,14 +1,11 @@
 <?php
 function DataBaseConnection($query){
-    ini_set('display_errors',1);
-    error_reporting(E_ALL);
     $link = mysql_connect("maki72.me", "ph", "ymi8ex");
     mysql_select_db("phoenix",$link);
     mysql_query("SET character_set_client = utf8");
     mysql_query("SET character_set_connection = utf8");
     mysql_query("SET character_set_results = utf8");
     $sql = mysql_query($query) or die(mysql_error());
-    mysql_error();
     mysql_close();
     return $sql;
 }
